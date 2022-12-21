a47561b99e09ff1c5ff60bbc8e39bb17fff7c5ef
Ruben Condoy <rubenaleajandro@gmail.com>
2022-12-21 14:49:00 -0500

    En clases

--- Ejemplosc3/src/paquete01/Ejemplo021.java
+++ Ejemplosc3/src/paquete01/Ejemplo021.java
@@ -9,7 +9,7 @@ package paquete01;
  *
  * @author reroes
  */
-public class Ejemplo02 {
+public class Ejemplo021 {
 
     /**
      * @param args the command line arguments
@@ -17,21 +17,28 @@ public class Ejemplo02 {
     public static void main(String[] args) {
         // Creación de arreglo bidimensionales
         
+        String cadenaFinal = "";
+        
         // forma 1
         int[][] arreglo1 = new int[3][4];
-        System.out.printf("Uso de la opción length: %d\n", arreglo1.length); // uso de la opcion lenght 3, por la fila
+       // System.out.printf("Uso de la opción length: %d\n", arreglo1.length); // uso de la opcion lenght 3, por la fila
         
-        System.out.printf("Valor en la posición [0][2]\n", arreglo1[0][2]); // para mostrar el valor de a posicion
+       // System.out.printf("Valor en la posición [0][2]\n", arreglo1[0][2]); // para mostrar el valor de a posicion
        
         arreglo1[0][2] = 1000;
         
                
         for (int fila = 0; fila < arreglo1.length; fila++) { // ciclo repetitivo contador llamado fila
             for (int col = 0; col < arreglo1[fila].length; col++) {
-                System.out.printf("fila[%d] columna[%d] = %d\n", 
-                        fila, col, arreglo1[fila][col]);
+               // System.out.printf("fila[%d] columna[%d] = %d\n", 
+               //        fila, col, arreglo1[fila][col]);
+               
+               cadenaFinal = String.format("%sfila[%d] columna[%d] = %d ♡\n" , // el pirmer % con fila y el otro con col
+                       cadenaFinal, fila , col , arreglo1[fila][col]);
             }
         }
+        
+        System.out.printf("%s\n",cadenaFinal);
         
     }
     
